a97d128b0452b458e1b1a3e1e8910ece70a43a40
João Justo <jpjustonunes@hotmail.com>
2015-08-03 16:42:58 +0100

    Schedule style

--- app/scripts/maps.js
+++ app/scripts/maps.js
@@ -2,15 +2,26 @@
   'use strict';
 
   function initialize() {
-    var coordiates = { lat: 40.998340, lng: -8.171281 };
+    var position = { lat: 40.998340, lng: -8.171281 };
     var mapOptions = {
       zoom: 16,
-      center: coordiates,
+      center: position,
       scrollwheel: false,
       mapTypeId: google.maps.MapTypeId.HYBRID
     };
 
-    return new google.maps.Map(document.getElementById('Map'), mapOptions);
+    var map = new google.maps.Map(document.getElementById('Map'), mapOptions);
+
+    var logo = {
+      url: 'images/logo_small.png',
+      size: new google.maps.Size(120, 64)
+    };
+
+    var beachMarker = new google.maps.Marker({
+        position: position,
+        map: map,
+        icon: logo
+    });
   }
 
   google.maps.event.addDomListener(window, 'load', initialize);
